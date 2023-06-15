d85b9184e38c1ef11790591b824444d5c61ac4cd
anhvt <anhvt@bssd.vn>
2023-06-15 14:59:39 +0700

    router

--- src/server.js
+++ src/server.js
@@ -2,15 +2,14 @@ import express from 'express'
 import configViewEngine from './configs/viewEngine';
 import dotenv from 'dotenv';
 dotenv.config();
+import initWebRoute from './route/web'
 
 var app = express();
 var port = process.env.PORT || 3001;
 
 configViewEngine(app);
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-})
+initWebRoute(app);
 
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`)
