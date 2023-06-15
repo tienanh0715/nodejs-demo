9503d130792975cb96969c2eb7e1bd62f295b1f9
anhvt <anhvt@bssd.vn>
2023-06-15 14:03:15 +0700

    static files

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
-import express from "express";
-import configViewEngine from "./configs/viewEngine";
+import express from 'express'
+import configViewEngine from './configs/viewEngine';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express();
-const port = 8080
+var app = express();
+var port = process.env.PORT || 3001;
 
 configViewEngine(app);
 
 app.get('/', (req, res) => {
-    res.render("index.ejs");
+    res.render('index.ejs');
 })
 
 app.listen(port, () => {
-    console.log(`Server is listenning at http://localhost:${port}`)
+    console.log(`Server is listening at http://localhost:${port}`)
 })
