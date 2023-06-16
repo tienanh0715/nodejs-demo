5054c1f5ca0d6af1f76d9459867d9a01e9550887
anhvt <anhvt@bssd.vn>
2023-06-16 11:05:00 +0700

    nav bar

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -6,18 +6,17 @@ let getHomePage = (req, res) => {
     connection.query(
         'SELECT * FROM `users` ',
         function (err, results, fields) {
-            console.log('---> check mysql: ');
             console.log(results); // results contains rows returned by server
             results.map((row) => {
                 data.push({
                     id: row.id,
                     firstName: row.first_name,
-                    latsName: row.last_name,
+                    lastName: row.last_name,
                     email: row.email,
                     address: row.address
                 })
             })
-            return res.render('index.ejs', { dataUser: JSON.stringify(data) });
+            return res.render('index.ejs', { dataUser: data });
         }
 
     );
