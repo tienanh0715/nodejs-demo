d4231ddc9e4a5c316b1c4c97a90858802a5bde94
anhvt <anhvt@bssd.vn>
2023-06-20 15:32:35 +0700

    upload_single_file

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,5 @@
 import pool from '../configs/connectDB'
+import multer from 'multer';
 
 let getHomePage = async (req, res) => {
     let data = [];
@@ -53,11 +54,43 @@ let updateUser = async (req, res) => {
     return res.redirect('/');
 }
 
+let getUploadFilePage = (req, res) => {
+    return res.render('uploadFile.ejs');
+}
+
+const upload = multer().single('profile_pic');
+
+let uploadProfilePic = async (req, res) => {
+    console.log(req.file)
+    upload(req, res, function (err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+    });
+}
+
 module.exports = {
     getHomePage,
     getDetailPage,
     createNewUser,
     deleteUser,
     getEditPage,
-    updateUser
+    updateUser,
+    getUploadFilePage,
+    uploadProfilePic
 }
